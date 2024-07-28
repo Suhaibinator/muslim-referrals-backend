a995468de44c1cce98ab64176eb6a250ea749131
Suhaibinator <42899065+Suhaibinator@users.noreply.github.com>
2024-07-28 09:20:58 -0400

    Added skeleton for API routes (#6)

---
+++ api/requester_routes.go
@@ -0,0 +1,23 @@
+package api
+
+import "net/http"
+
+func CreateRequesterHandler(w http.ResponseWriter, r *http.Request) {
+	// Implement the logic to create a requester
+}
+
+func GetRequesterHandler(w http.ResponseWriter, r *http.Request) {
+	// Implement the logic to get a requester by requester_id
+}
+
+func UpdateRequesterHandler(w http.ResponseWriter, r *http.Request) {
+	// Implement the logic to update a requester by requester_id
+}
+
+func DeleteRequesterHandler(w http.ResponseWriter, r *http.Request) {
+	// Implement the logic to delete a requester by requester_id
+}
+
+func UploadResumeHandler(w http.ResponseWriter, r *http.Request) {
+	// Implement the logic to upload a resume
+}
