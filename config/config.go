12e2f16a9dee46a9f2e66b594ce49638674ed55c
suhaibinator <suhaib.abdulquddos@gmail.com>
2024-10-12 21:25:47 -0400

    Support for test env

--- config/config.go
+++ config/config.go
@@ -12,20 +12,24 @@ var (
 	GoogleOauthConfig *oauth2.Config
 )
 
+const (
+	NEW_USER_SIGNUP_PATH = "/app/new-user-signup"
+	DEFAULT_LOGIN_PATH   = "/app/"
+
+	Port = "80"
+
+	OAuthRedirectPath     = "/login"
+	OauthRedirectHost     = "https://muslimreferrals.xyz"
+	OauthRedirectHostTest = "http://localhost:3000"
+)
+
 func init() {
 	DatabasePath = os.Getenv("SQLITE_DB_PATH")
 	GoogleOauthConfig = &oauth2.Config{
-		RedirectURL:  "https://muslimreferrals.xyz/login",
+		RedirectURL:  OauthRedirectHostTest + OAuthRedirectPath,
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
 		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
 		Endpoint:     google.Endpoint,
 	}
 }
-
-const (
-	NEW_USER_SIGNUP_PATH = "/app/new-user-signup"
-	DEFAULT_LOGIN_PATH   = "/app/"
-
-	Port = "80"
-)
